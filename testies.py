98808098c58d2405f9eeea83a661bb68f8aa8287
Lane Katris <lanekatris@gmail.com>
2018-08-03 19:26:03 -0400

    trying to get a handle on this micropython and c++ craziness

---
+++ testies.py
@@ -0,0 +1,66 @@
+import time
+#import board
+#import busio
+import adafruit_gps
+#from pyb import UART
+from machine import UART
+
+print("starting gps code")
+
+#RX = 12
+#TX = 27
+RX = 16
+TX = 17
+
+#uart = UART(TX, RX, 9600, 3000)
+uart = UART(1, 9600)                         # init with given baudrate
+uart.init(9600, bits=8, parity=None, stop=1) 
+
+gps = adafruit_gps.GPS(uart)
+
+gps.send_command('PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
+gps.send_command('PMTK220,1000')
+
+last_print = time.time()
+while True:
+    # Make sure to call gps.update() every loop iteration and at least twice
+    # as fast as data comes from the GPS unit (usually every second).
+    # This returns a bool that's true if it parsed new data (you can ignore it
+    # though if you don't care and instead look at the has_fix property).
+    gps.update()
+    # Every second print out current location details if there's a fix.
+    current = time.time()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            # Try again if we don't have a fix yet.
+            print('Waiting for fix...')
+            continue
+        # We have a fix! (gps.has_fix is true)
+        # Print out details about the fix like location, date, etc.
+        print('=' * 40)  # Print a separator line.
+        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
+            gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
+            gps.timestamp_utc.tm_mday,  # struct_time object that holds
+            gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+            gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+            gps.timestamp_utc.tm_min,   # month!
+            gps.timestamp_utc.tm_sec))
+        print('Latitude: {} degrees'.format(gps.latitude))
+        print('Longitude: {} degrees'.format(gps.longitude))
+        print('Fix quality: {}'.format(gps.fix_quality))
+        # Some attributes beyond latitude, longitude and timestamp are optional
+        # and might not be present.  Check if they're None before trying to use!
+        if gps.satellites is not None:
+            print('# satellites: {}'.format(gps.satellites))
+        if gps.altitude_m is not None:
+            print('Altitude: {} meters'.format(gps.altitude_m))
+        if gps.track_angle_deg is not None:
+            print('Speed: {} knots'.format(gps.speed_knots))
+        if gps.track_angle_deg is not None:
+            print('Track angle: {} degrees'.format(gps.track_angle_deg))
+        if gps.horizontal_dilution is not None:
+            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+        if gps.height_geoid is not None:
+            print('Height geo ID: {} meters'.format(gps.height_geoid))
