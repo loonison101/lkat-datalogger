8e676f9ed39e7710ee9f6b1514416a1257c9c6f8
Lane Katris <lanekatris@gmail.com>
2018-08-08 19:36:58 -0400

    Successfully writing gps data to SD card

--- src/main.cpp
+++ src/main.cpp
@@ -757,10 +757,12 @@ const int chipSelect = 33;
 HardwareSerial MySerial(1);
 TinyGPSPlus gps;
 uint32_t nextSerialTaskTs = 0;
-#define TASK_SERIAL_RATE 2000
+#define TASK_SERIAL_RATE 10000
+
+// char dataRow[1000];
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
     MySerial.begin(9600, SERIAL_8N1, 16, 17);
 
     pinMode(chipSelect, OUTPUT);
@@ -902,7 +904,8 @@ static void printStr(const char *str, int len)
 
 void displayInfo()
 {
-printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+    // Serial.println("custom satellites: " + gps.satellites.value());
+    printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
   printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
   printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
   printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
@@ -911,32 +914,63 @@ printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
   printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
   printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
   printFloat(gps.speed.mph(), gps.speed.isValid(), 6, 2);
+
   printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "*** ", 6);
 
   Serial.println();
+  char buffer[1000];
+  sprintf(buffer, "%ld,%0.2f,%f, %f,%ld,%02d/%02d/%02d,%02d:%02d:%02d,%f,%f", 
+        gps.satellites.value(),
+        gps.hdop.hdop(),
+        gps.location.lat(),
+        gps.location.lng(),
+        gps.location.age(),
+        
+        gps.date.month(),
+        gps.date.day(),
+        gps.date.year(),
+
+        gps.time.hour(),
+        gps.time.minute(),
+        gps.time.second(),
+
+        gps.altitude.meters(),
+        gps.speed.mph()
+
+
+
+        );
+
+    // Serial.println("custom -- " + String(buffer));
+        Serial.println("trying to write to sd card");
+        logFile = SD.open("log.txt", FILE_WRITE);
+
+        if (logFile) {
+            Serial.println("writing to log.txt");
+            logFile.println(String(buffer));
+            logFile.close();
+            Serial.println("done writing");
+        } else {
+            Serial.println("error opening log.txt");
+        }
+
 }
 void loop() {
     while (MySerial.available() > 0) {
         gps.encode(MySerial.read());
     }
 
+    // while (MySerial.available() > 0) {
+    //     uint8_t byteFromSerial = MySerial.read();
+    //     Serial.write(byteFromSerial);
+    // }
+
     if (nextSerialTaskTs < millis()) {
-            //uint8_t byteFromSerial = MySerial.read();
-            //Serial.write(byteFromSerial);
+            
             // Do something
             //if (gps.encode(byteFromSerial)) {
-                //displayInfo();
-                Serial.println("trying to write to sd card");
-                logFile = SD.open("log.txt", FILE_WRITE);
-
-                if (logFile) {
-                    Serial.println("writing to log.txt");
-                    logFile.println("big long sting man");
-                    logFile.close();
-                    Serial.println("done writing");
-                } else {
-                    Serial.println("error opening log.txt");
-                }
+                displayInfo();
+                
                 
                 
             // } else {
